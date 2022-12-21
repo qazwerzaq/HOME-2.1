bf2043245c4c95db9dfec3eea4cb4e8816a93c5b
qazwerzaq <120454546+qazwerzaq@users.noreply.github.com>
2022-12-21 21:09:19 +0300

    дз1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine ("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine());
+int answer = 0;
+if(num >= 100 && num <= 999)
+    {
+    int a = num / 100;
+    int b = (num / 10) % 10;
+    int c = num % 10;
+    answer = a * 10 + c;
+    Console.WriteLine(answer);
+    }
+else
+    {
+    Console.WriteLine("Вы ввели неправильное число");
+    }
+
+
